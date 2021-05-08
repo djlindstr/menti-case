7cdcd2436dec7f2999ba33e76d1aa8e1244d6df3
Jonas Lindström <jonaslindstr@gmail.com>
2021-05-08 13:12:37 +0200

    Get data from API endpoints and return as JSON

--- server.rb
+++ server.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby
 
 require 'webrick'
+require 'httparty'
+require 'json'
 
 PORT = ENV['LISTEN_PORT']
+UUID = '48d75c359ce4'
+ENDPOINTS = {
+  questions: "https://api.mentimeter.com/questions/#{UUID}",
+  results: "https://api.mentimeter.com/questions/#{UUID}/result",
+}
 
 class Backend < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(request, response)
-    puts request
-    response.status = 200
-    response.body = "Hello world"
+    question = HTTParty.get(ENDPOINTS[:questions]).find {|k, v| k == 'question'}
+    results = HTTParty.get(ENDPOINTS[:results]).find {|k, v| k == 'results'}
+    answer = [question, results].to_h
+    response.content_type = 'application/json'
+    response.body = JSON.generate(answer)
   end
 end
 
